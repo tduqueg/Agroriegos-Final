d68c2db03a3f182dc0b6058a9990f91755679a2e
tduqueg <tduqueg@eafit.edu.co>
2022-10-15 23:18:01 -0500

    Productos Aparecen en Home

--- src/Components/IndividualProduct.js
+++ src/Components/IndividualProduct.js
@@ -1,5 +1,18 @@
 import React from 'react';
 
-export default function IndividualProduct() {
-  return <div>IndividualProduct</div>;
+export default function IndividualProduct({ individualProduct }) {
+  console.log(IndividualProduct);
+  return (
+    <div className="product">
+      <div className="product-img">
+        <img src={individualProduct.url} alt="product-img" />
+      </div>
+      <div className="product-text title">{individualProduct.title}</div>
+      <div className="product-text description">
+        {individualProduct.description}
+      </div>
+      <div className="product-text price">$ {individualProduct.price}</div>
+      <div className="btn btn-danger btn-md cart-btn">Añadir al carrito</div>
+    </div>
+  );
 }
